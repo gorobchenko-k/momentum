5ed5c16c2934e87b7452655a0ce620b5ab0c3883
gorobchenko-k <gorobchenko.es@mail.ru>
2023-02-13 11:45:58 +0300

    feat: add clock and calendar

---
+++ momentum/scripts/time.js
@@ -0,0 +1,18 @@
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+
+function showDate(date) {
+
+    const currentDate = date.toLocaleDateString('ru-RU', { weekday: 'long', month: 'long', day: 'numeric' }); //'ru-RU' en-US
+    dateElement.textContent = currentDate;
+}
+
+function showTime() {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    timeElement.textContent = currentTime;
+    showDate(date);
+    setTimeout(showTime, 1000);
+}
+
+showTime();
